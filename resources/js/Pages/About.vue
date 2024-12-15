f6a712d7b07520426adc5e17a8bad2a546e3d183
BugaPunks <punksbuga@outlook.es>
2024-12-14 20:28:28 -0400

    Proyecto Iniciado

---
+++ resources/js/Pages/About.vue
@@ -0,0 +1,11 @@
+<script setup>
+defineProps({
+    user: String
+})
+</script>
+<template>
+    <Head title=" | Acerca"/>
+    <div>
+        <h1>Acerca de {{ user }}</h1>
+    </div>
+</template>
---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,7 @@
+<script setup>
+
+</script>
+<template>
+    <Head title=" | Inicio" />
+    <h1>Hola mundo</h1>
+</template>
