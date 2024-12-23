e65c4a6819660050c123b923f1bb50ce230e9a35
BugaPunks <punksbuga@outlook.es>
2024-12-23 10:35:15 -0400

    [ADD] Register.vue

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -4,5 +4,6 @@
 <template>
     <Head :title="` | ${$page.component}`" />
     <h1>Bienvenido {{ $page.props.user }}</h1>
-    <Link class=" mt-56 -mb-0 block" href="/" preserve-scroll>Refrescar</Link>
+    <h1>Pagina de Inicio</h1>
+    <Link class="block" href="/" preserve-scroll>Refrescar</Link>
 </template> 
